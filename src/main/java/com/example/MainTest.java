2fe56e7dc77eda4e40e7ce5ed80b16104fcfa907
andyluo882016 <andylu882016>
2022-02-06 12:02:29 -0800

    commit -v0 developer branch @ 2/5

--- src/main/java/com/example/MainTest.java
+++ src/main/java/com/example/MainTest.java
@@ -4,5 +4,6 @@ public class MainTest {
 
     public static void main(String[] args){
         System.out.println(" Show My GitHub Testing");
+        System.out.println("this developer1 branch");
     }
 }
