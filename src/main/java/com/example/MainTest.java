81e56211cbd7a4bb1794bbaae95a2080a442fcc5
andyluo882016 <andylu882016>
2022-02-06 13:00:31 -0800

    commit -v6 developer branch @ 2/6

--- src/main/java/com/example/MainTest.java
+++ src/main/java/com/example/MainTest.java
@@ -4,6 +4,6 @@ public class MainTest {
 
     public static void main(String[] args){
         System.out.println(" Show My GitHub Testing");
-        System.out.println("this developer1 branch making changing1");
+        System.out.println("this developer1 branch making changing-1");
     }
 }
