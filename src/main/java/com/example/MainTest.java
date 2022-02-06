1747bbd8ffc913cd53526f0e55b6190728f235de
andyluo882016 <andylu882016>
2022-02-05 23:09:24 -0800

    commit -v3 @2/4

--- src/main/java/com/example/MainTest.java
+++ src/main/java/com/example/MainTest.java
@@ -3,6 +3,6 @@ package com.example;
 public class MainTest {
 
     public static void main(String[] args){
-        System.out.println(" Show My GitHub Testing");
+        System.out.println(" Show My GitHub Testing!!");
     }
 }
