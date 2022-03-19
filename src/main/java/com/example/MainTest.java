59d2bba6bb7a5a4bce376b155300a8fde9c6aa02
andyluo882016 <andylu882016@gmail.com>
2022-03-19 14:36:32 -0700

    commit -v1 test2 for mytest2 branch @ 3/19

--- src/main/java/com/example/MainTest.java
+++ src/main/java/com/example/MainTest.java
@@ -1,9 +1,16 @@
 package com.example;
 
+import com.example.com.domain.Employee;
+
 public class MainTest {
 
     public static void main(String[] args){
         System.out.println(" Show My GitHub Testing");
         System.out.println("this developer1 branch making changing-1");
+        System.out.println(getmeployee().toString());
+    }
+
+    public static Employee getmeployee(){
+        return new Employee("MNJU", 29, "Keven");
     }
 }
